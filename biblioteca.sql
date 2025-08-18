010ba5967a5b59b291705249c7eca1aa7b99ada9
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2025-08-18 09:11:38 -0300

    Add files via upload

---
+++ biblioteca.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 18-Ago-2025 às 14:10
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `id` int(11) NOT NULL,
+  `autor` varchar(255) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `genero` varchar(255) NOT NULL,
+  `ano` int(4) NOT NULL,
+  `paginas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `livros`
+--
+
+INSERT INTO `livros` (`id`, `autor`, `titulo`, `genero`, `ano`, `paginas`) VALUES
+(1, 'Suzanne Collins', 'Amanhecer na Colheita', 'Distopia', 2025, 448),
+(2, 'William Golding', 'Senhor das Moscas', 'Ficção', 1954, 215),
+(3, 'Jane Austen', 'Orgulho e Preconceito', 'Romance', 1813, 288),
+(4, 'Rick Riordan', 'A Maldição do Titã', 'Fantasia', 2007, 336),
+(5, 'Stephen King', 'O Iluminado', 'Horror', 1977, 520);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
