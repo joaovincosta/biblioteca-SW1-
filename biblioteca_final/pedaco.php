fa04f160d55699055efbb6c29a5131ea48f24685
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2025-09-25 09:06:05 -0300

    Add files via upload

---
+++ biblioteca_final/pedaco.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Biblioteca João Vinícius Costa</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <div class="container">
+      <h1 class="mb-0">BIBLIOTECA João Vinícius Costa</h1>
+    </div>
+  </header>
+  <div class="container my-4">
+    <!-- aqui vai o resto -->
+  </div>
+</body>
+</html>
